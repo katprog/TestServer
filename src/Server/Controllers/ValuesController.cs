953a4b86f8e0a0b54ae6f8d9021c905f9026cbc0
one <one@HOME>
2017-06-18 20:01:08 +0700

    del

--- src/Server/Controllers/ValuesController.cs
+++ src/Server/Controllers/ValuesController.cs
@@ -9,7 +9,6 @@ namespace Server.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // 1
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
